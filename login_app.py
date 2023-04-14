f94c0c1f54fc33b1a5e6e1f15ed877ac3503c5dc
BillyHack <91779980+KuanlinBilly@users.noreply.github.com>
2023-04-14 14:08:23 +0800

    Add custom background image and GitHub link to login page

--- login_app.py
+++ login_app.py
@@ -1,7 +1,41 @@
-# add login page for app3
 import streamlit as st
 import hashlib
 from predictor_app import * 
+
+# Function to set the background image
+def set_background_image(image_url):
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background: url("{image_url}") no-repeat center center fixed;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }}
+        .stApp .stMarkdown h1, .stApp .stMarkdown h2, .stApp .stMarkdown p {{
+            color: black;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    
+def reset_background():
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background: none;
+        }}
+        .stApp .stMarkdown h1, .stApp .stMarkdown h2, .stApp .stMarkdown p {{
+            color: inherit;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
 # Function to verify the user's password (you should replace this with your actual authentication method)
 def verify_password(stored_password, provided_password):
@@ -9,6 +43,10 @@ def verify_password(stored_password, provided_password):
 
 # Define a function to display the login page
 def show_login_page(placeholders):
+    # Set the background image here
+    picture_url = "https://i.postimg.cc/t45ryBwW/Adobe-Stock-328664888.jpg"
+    set_background_image(picture_url)  # Change the URL to your custom image
+
     my_username = "ndhu"
     my_password = "1234"
     placeholders[0].title("Welcome!")
@@ -30,9 +68,15 @@ def show_login_page(placeholders):
         else:
             placeholders[6].error("Incorrect username or password")
             return False
+    # Add a new placeholder for the GitHub hyperlink
+    placeholders[7].markdown(
+        f'<p style="text-align: center; color: black; margin-top: 15px;"><a href="https://github.com/KuanlinBilly" target="_blank">Click here</a> to see my GitHub</p>',
+        unsafe_allow_html=True,
+    )
 
 # Define a function to display the main content of the web app
 def show_main_content():
+    reset_background()  # Add this line to reset the background
     implied_volatility_predictor()
 
 # Main function to display the login page or the main content depending on the login status
@@ -41,7 +85,7 @@ def main():
         st.session_state.login_status = False
 
     if not st.session_state.login_status:
-        placeholders = [st.empty() for _ in range(7)]
+        placeholders = [st.empty() for _ in range(8)]
         st.session_state.login_status = show_login_page(placeholders)
 
     if st.session_state.login_status:
