59dd1cdde183ce855b14363beffe8f51f2d9e40a
BillyHack <91779980+KuanlinBilly@users.noreply.github.com>
2023-04-22 20:10:42 +0800

    Update predictor_app.py

--- predictor_app.py
+++ predictor_app.py
@@ -212,7 +212,7 @@ def implied_volatility_predictor():
     
     # Add title and description
     st.title('Implied Volatility Predictor')
-    st.text('By Kuanlin Lai')
+    #st.text('By Kuanlin Lai')
     st.markdown("##### You can customize and train a neural network for implied volatility prediction using this web app!")
     
     
